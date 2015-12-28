806f823a2817b84eaaf26284f783ae6ddd1f6ded
Stefan Aleksic <stefan@stefanaleksic.com>
2015-12-28 17:23:55 -0500

    Create script and add output files

--- solve.py
+++ solve.py
@@ -1,4 +1,7 @@
 import csv
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 def is_number(c):
     try:
@@ -19,8 +22,34 @@ def get_amount_chars_together(st):
             previous_was_char = False
     return total
 
-with open('all.csv', 'rb') as f:
-    rea = csv.reader(f, delimiter=',')
-    for row in rea:
-        all_posts = row[2].split()
-        print str(map(get_amount_chars_together, all_posts))
+def get_index_biggest_list(total_list):
+    biggest_len = 0 
+    biggest_index = -1
+    for index, item in enumerate(total_list):
+        if len(item) > biggest_len:
+            biggest_index = index
+            biggest_len = len(item)
+    return biggest_index
+
+def main():
+    total_lists = []
+    with open('all.csv', 'rb') as f:
+        rea = csv.reader(f, delimiter=',')
+        for row in rea:
+            all_posts = row[2].split()
+            total_lists.append(map(get_amount_chars_together, all_posts))
+
+    biggest_list_index = get_index_biggest_list(total_lists)
+    elements = [[] for x in range(0, len(total_lists[biggest_list_index]))]
+    for l in total_lists:
+        for x in range(0, len(total_lists[biggest_list_index])):
+            if len(l) > x:
+                elements[x].append(l[x])
+
+    for index, element in enumerate(elements):
+        plt.hist(element, bins = 50,range = (0,20), alpha = 0.75)
+        plt.savefig('out/' + str(index))
+        plt.clf()
+
+if __name__ == '__main__':
+    main()
